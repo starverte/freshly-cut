5df845e4eb12002a9495925d7b4749487e620c19
Matt Beall <mbeall@starverte.com>
2016-06-18 16:34:53 -0600

    Design: New Front Page layout completed

--- header-nav.php
+++ header-nav.php
@@ -14,7 +14,6 @@
     <h1 class="screen-reader-text"><?php esc_html_e( 'Menu', 'flint' ); ?></h1>
     <div class="screen-reader-text skip-link"><a href="#content" title="<?php esc_attr_e( 'Skip to content', 'flint' ); ?>"><?php esc_html_e( 'Skip to content', 'flint' ); ?></a></div>
     <div class="container">
-      <!-- Brand and toggle get grouped for better mobile display -->
       <div class="navbar-header">
         <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-primary">
           <span class="sr-only">Toggle navigation</span>
@@ -22,11 +21,12 @@
           <span class="icon-bar"></span>
           <span class="icon-bar"></span>
         </button>
+        <a class="navbar-brand hidden-sm hidden-md hidden-lg" href="<?php echo esc_url( home_url() ); ?>"><?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?></a>
       </div><!-- .navbar-header -->
 
-      <!-- Collect the nav links, forms, and other content for toggling -->
       <div class="collapse navbar-collapse navbar-primary">
         <?php wp_nav_menu( array( 'theme_location' => 'primary', 'container' => false, 'menu_class' => 'nav navbar-nav', 'fallback_cb' => 'flint_nav_fallback', 'walker' => new Flint_Walker_Nav_Menu_Navbar ) ); ?>
+        <span class="navbar-text navbar-right" style="color:#fff;font-weight:bold;">970.800.1829</span>
       </div><!-- .navbar-collapse -->
     </div><!-- .container -->
   </nav><!-- .navbar -->
